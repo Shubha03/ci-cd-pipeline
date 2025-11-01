9dc760274ed1b1495ba803a360e225d420da3ad2
Shubha Barman <shubhab501@gmail.com>
2025-11-01 07:43:43 +0530

    ci-cd pipeline without deploying in kubernates

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "✅ Flask app deployed via GitHub Actions, Docker, and Kubernetes!"
+def home():
+    return "✅ Hello, Flask is running inside Minikube!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
